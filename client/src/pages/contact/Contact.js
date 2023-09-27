ad9fb2dffe3758eb0674fdd83ed025f12dd0f9e7
Paulsig007 <131915869+Paulsig007@users.noreply.github.com>
2023-09-27 12:17:29 -0600

    add emailjs form, with mobile-responsive styling

--- client/src/pages/contact/Contact.js
+++ client/src/pages/contact/Contact.js
@@ -1,49 +1,87 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import styles from "./Contact.module.css";
+import Grid from "@mui/material/Grid";
 
 function Contact() {
-    const form = useRef();
+  const form = useRef();
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-        emailjs.sendForm(
-            "service_3z6xw3o",
-            "template_3z6xw3o",
-            form.current,
-            "user_3z6xw3o"
-        ).then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-    };
+    emailjs
+      .sendForm(
+        "service_xoptlf6",
+        "template_dvg0ptr",
+        form.current,
+        "Z9H-fpcVpqK5dqCqm"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
-    return (
-        <div className={styles.componentContainer}>
-        <div className={styles.contactBackground}>
+  return (
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Grid
+        item
+        sm={12}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+        }}
+      >
         <h1 className={styles.contactTitle}>Contact Me</h1>
+      </Grid>
+      <Grid item sm={12}>
         <p className={styles.contactText}>
           If you have any questions, or just want to talk shop, please fill out
           the form below and I will get back to you as soon as possible.
         </p>
-        <form ref={form} onSubmit={sendEmail}>
+      </Grid>
+      <form ref={form} onSubmit={sendEmail}>
+        <Grid item sm={12}>
           <label className={styles.conLabel}>Name</label>
+        </Grid>
+        <Grid item sm={12}>
           <input type="text" name="user_name" placeholder="Name" />
+        </Grid>
+        <Grid item sm={12}>
           <label className={styles.conLabel}>Email</label>
+        </Grid>
+        <Grid item sm={12}>
           <input type="email" name="user_email" placeholder="Email" />
+        </Grid>
+        <Grid item sm={12}>
           <label className={styles.conLabel}>Message</label>
+        </Grid>
+        <Grid item sm={12}>
           <textarea
             id="contMessage"
             name="message"
             placeholder="How can I assist you?"
           />
-          <input className="sendBtn" type="submit" value="Send" />
-        </form>
-      </div>
-      </div>
-    );
+        </Grid>
+        <Grid item sm={12}>
+          <input type="submit" value="Send" />
+        </Grid>
+      </form>
+    </Grid>
+  );
 }
 
 export default Contact;
